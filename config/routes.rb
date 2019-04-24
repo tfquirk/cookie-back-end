3ddd50b84ff0ebb33ab381083619ab1e2e5e102b
tfquirk <timothyfquirk@gmail.com>
2019-04-24 17:04:48 -0400

    added namespaced routes for recipies, ingredients, and the join

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :recipes, only: [:index, :show]
+      resources :ingredients, only: [:index, :show]
+      resources :recipe_ingredients, only: [:index, :show]
+     end
+   end
+
+
 end
